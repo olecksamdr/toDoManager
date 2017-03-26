fe521cfa99ba1c7cc8142c74f190454bb5f30cc2
Ksardarion Raota <cool.ksandr2013@gmail.com>
2017-03-26 17:51:44 +0300

    All must work

--- sys/initDataFromDB.php
+++ sys/initDataFromDB.php
@@ -16,13 +16,13 @@ if (isset($_GET['taskId'])) {
 	$currentTask->execute(Array($taskId));
 }
 //Get data for list
-if (isset($_GET['listId'])) {
-	$listId = $_GET['listId'];
-	$qForList = "SELECT * FROM `lists` WHERE `id` = ?";
-	$list = $db->prepare($qForList);
-	$list->execute(Array($listId));
-	$dataFromList = $list->fetch();
-}
+// if (isset($_GET['listId'])) {
+// 	$listId = $_GET['listId'];
+// 	$qForList = "SELECT * FROM `lists` WHERE `id` = ?";
+// 	$list = $db->prepare($qForList);
+// 	$list->execute(Array($listId));
+// 	$dataFromList = $list->fetch();
+// }
 //Get data about user
 $qForUser = "SELECT * FROM `users` WHERE `id` = ?";
 $userPrepare = $db->prepare($qForUser);
