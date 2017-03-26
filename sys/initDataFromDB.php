03eac4339ccb447bcad43e1c40c82536c5c0060c
Ksardarion Raota <cool.ksandr2013@gmail.com>
2017-03-26 17:33:09 +0300

    Crearing initDataFromDB.php

--- sys/initDataFromDB.php
+++ sys/initDataFromDB.php
@@ -1,12 +1,12 @@
 <?php
 
 //Get data for tasks
-if (isset($_GET['listId'])) {
-	$listId = $_GET['listId'];
-	$qForTask = "SELECT `tasks`.`id`, `tasks`.`title`, `tasks`.`description` FROM `lists` LEFT JOIN `tasks` ON (`lists`.`id` = `tasks`.`listId`) WHERE `lists`.`id` = ? ORDER BY `id` DESC";
-	$task = $db->prepare($qForTask);
-	$task->execute(Array($listId));
-}
+// if (isset($_GET['listId'])) {
+// 	$listId = $_GET['listId'];
+// 	$qForTask = "SELECT `tasks`.`id`, `tasks`.`title`, `tasks`.`description` FROM `lists` LEFT JOIN `tasks` ON (`lists`.`id` = `tasks`.`listId`) WHERE `lists`.`id` = ? ORDER BY `id` DESC";
+// 	$task = $db->prepare($qForTask);
+// 	$task->execute(Array($listId));
+// }
 
 //Get data for current task
 if (isset($_GET['taskId'])) {
